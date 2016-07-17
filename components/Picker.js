7cfcbc398ad1745138e1361564a0a8fce1157e0d
Lucas <lucas@agtlucas.com>
2016-07-17 19:22:09 -0300

    Fix Picker typo

--- components/Picker.js
+++ components/Picker.js
@@ -9,11 +9,11 @@ export default class Picker extends Component {
         <h1>{value}</h1>
         <select onChange={e => onChange(e.target.value)}
           value={value}>
-          {options.map(option => {
+          {options.map(option =>
             <option value={option} key={option}>
               {option}
-            </option>
-          })}
+            </option>)
+          }
         </select>
       </span>
     )
